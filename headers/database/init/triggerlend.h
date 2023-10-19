746b16ef89973887f4eba340fe240b109ca88123
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 21:14:10 +0300

    Class for lending trigger

---
+++ headers/database/init/triggerlend.h
@@ -0,0 +1,19 @@
+#ifndef TRIGGERLENDINIT_H
+#define TRIGGERLENDINIT_H
+
+#include "headers/database/init/dbinit.h"
+
+class TriggerLendInit
+{
+public: //Constructor
+    TriggerLendInit();
+
+public: //Method
+    void initTriggerLend();
+
+public: //Method
+    void TriggerLendCreate();
+
+};
+
+#endif // TRIGGERLENDINIT_H
