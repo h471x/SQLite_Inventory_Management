b8bdfb8d9638d5bcd5d98b81a224bea863a8d6e4
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 03:05:39 +0300

    Class of trigger save

---
+++ headers/database/init/triggersave.h
@@ -0,0 +1,20 @@
+#ifndef TRIGGERSAVEINIT_H
+#define TRIGGERSAVEINIT_H
+
+
+#include "headers/database/init/dbinit.h"
+
+class TriggerSaveInit
+{
+public: //Constructor
+    TriggerSaveInit();
+
+public: //Method
+    void initTriggerSave();
+
+public: //Method
+    void TriggerSaveCreate();
+
+};
+
+#endif // TRIGGERSAVEINIT_H
