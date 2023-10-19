608c84fcecf2c9b491890f7653ef11ad51969fc1
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 03:05:25 +0300

    Class of History table

---
+++ headers/database/init/dbhistory.h
@@ -0,0 +1,20 @@
+#ifndef HISTORYINIT_H
+#define HISTORYINIT_H
+
+
+#include "headers/database/init/dbinit.h"
+
+class HistoryInit
+{
+public: //Constructor
+    HistoryInit();
+
+public: //Method
+    void initHistory();
+
+public: //Method
+    void tableHistory();
+
+};
+
+#endif // HISTORYINIT_H
