f347065e9bbf274ec63bb031ce65d1dfd32bcef3
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 03:02:21 +0300

    Modified User Table

--- sources/database/init/dbuser.cpp
+++ sources/database/init/dbuser.cpp
@@ -9,14 +9,13 @@ void UserInit::initUser(){
 
 void UserInit::tableUser(){
     QSqlQuery query;
-    query.exec("CREATE TABLE IF NOT EXISTS UTILISATEUR (IdUtilisateur  INTEGER NOT NULL ,NomUtilisateur TEXT NOT NULL ,PrenomUtilisateur TEXT ,AdresseUtilisateur TEXT  NOT NULL ,TelephoneUtilisateur TEXT NOT NULL,PRIMARY KEY(IdUtilisateur));");
+    query.exec("CREATE TABLE IF NOT EXISTS UTILISATEUR (UsernameUtilisateur TEXT NOT NULL UNIQUE, NomUtilisateur TEXT NOT NULL , PrenomUtilisateur TEXT , AdresseUtilisateur TEXT  NOT NULL , TelephoneUtilisateur TEXT NOT NULL,PRIMARY KEY(UsernameUtilisateur));");
 
 }
 
 void UserInit::valueUser(){
     QSqlQuery query;
-    query.prepare("INSERT OR IGNORE INTO UTILISATEUR(IdUtilisateur,NomUtilisateur,PrenomUtilisateur,AdresseUtilisateur,TelephoneUtilisateur) VALUES(:idusr, :nusr, :pusr, :adr, :phn)");
-    query.bindValue(":idusr", 8);
+    query.prepare("INSERT OR IGNORE INTO UTILISATEUR(NomUtilisateur,PrenomUtilisateur,AdresseUtilisateur,TelephoneUtilisateur) VALUES(:idusr, :nusr, :pusr, :adr, :phn)");
     query.bindValue(":nusr", "Hatix");
     query.bindValue(":pusr", "Ntsoa");
     query.bindValue(":adr", "Fianarantsoa");
