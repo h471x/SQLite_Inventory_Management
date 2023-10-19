c3a1abd8fac743a0e3668a521dfad31c9bd9fddd
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 03:06:03 +0300

    Class declaration of history

---
+++ headers/database/show/history.h
@@ -0,0 +1,24 @@
+#ifndef HISTORYSHOW_H
+#define HISTORYSHOW_H
+
+#include "headers/database/init/dbinit.h"
+
+class HistoryShow : public QWidget
+{
+public: //Constructor
+    HistoryShow();
+
+public: //Method
+    void showHistory(QTableWidget* productTable);
+    void setStyleHistory(QTableWidget* productTable, int tableColumnCount, int tableRowCount, QSqlTableModel* model);
+
+private: //Object Instance
+    QSqlDatabase mydb;
+
+private: //actions buttons
+    QFont tableFont;
+    QString handleItemSelectionChanged(QTableWidget* productTable);
+
+};
+
+#endif // HISTORYSHOW_H
