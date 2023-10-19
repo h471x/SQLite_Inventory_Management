b45d409ed08b0877fcc436a2102ded344a76af72
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 21:14:25 +0300

    Class for trigger return

---
+++ headers/database/init/triggerreturn.h
@@ -0,0 +1,19 @@
+#ifndef TRIGGERRETURNINIT_H
+#define TRIGGERRETURNINIT_H
+
+#include "headers/database/init/dbinit.h"
+
+class TriggerReturnInit
+{
+public: //Constructor
+    TriggerReturnInit();
+
+public: //Method
+    void initTriggerReturn();
+
+public: //Method
+    void TriggerReturnCreate();
+
+};
+
+#endif // TRIGGERRETURNINIT_H
