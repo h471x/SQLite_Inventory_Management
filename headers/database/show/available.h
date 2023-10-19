60d7d68639078a92065436da21434aac76e8ce42
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 16:41:55 +0300

    Available Product header class

---
+++ headers/database/show/available.h
@@ -0,0 +1,26 @@
+#ifndef AVAILABLESHOW_H
+#define AVAILABLESHOW_H
+
+#include "headers/database/init/dbinit.h"
+#include "headers/database/show/table.h"
+
+class AvailableShow : public QWidget
+{
+public: //Constructor
+    AvailableShow();
+
+public: //Method
+    void showAvailable(QTableWidget* productTable);
+    void setStyleAvailable(QTableWidget* productTable, int tableColumnCount, int tableRowCount, QSqlTableModel* model);
+//    void AvailablebtnConfig();
+
+private: //Object Instance
+    QSqlDatabase mydb;
+
+private: //actions buttons
+    QFont tableFont;
+//    QString handleItemSelectionChanged(QTableWidget* productTable);
+
+};
+
+#endif // AVAILABLESHOW_H
