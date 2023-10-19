80e9352f1dfe370559b139d846093e5e60d2e629
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 03:07:00 +0300

    Database of History

---
+++ sources/database/init/dbhistory.cpp
@@ -0,0 +1,12 @@
+#include "headers/database/init/dbhistory.h"
+
+HistoryInit::HistoryInit(){}
+
+void HistoryInit::initHistory(){
+    tableHistory();
+}
+
+void HistoryInit::tableHistory(){
+    QSqlQuery query;
+    query.exec("CREATE TABLE IF NOT EXISTS REQUETE (IdRequete INTEGER NOT NULL PRIMARY KEY,TypeRequete TEXT NOT NULL ,DHRequete TEXT NOT NULL ,UsernameAdmin TEXT NOT NULL,UsernameUtilisateur TEXT NOT NULL,NomMateriel TEXT NOT NULL);");
+}
