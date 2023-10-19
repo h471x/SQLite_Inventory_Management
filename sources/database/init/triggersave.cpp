80ab650299d9da845ebba14044527ed5de1b5982
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-19 03:07:34 +0300

    Trigger Save implementation

---
+++ sources/database/init/triggersave.cpp
@@ -0,0 +1,12 @@
+#include "headers/database/init/triggersave.h"
+
+TriggerSaveInit::TriggerSaveInit(){}
+
+void TriggerSaveInit::initTriggerSave(){
+    TriggerSaveCreate();
+}
+
+void TriggerSaveInit::TriggerSaveCreate(){
+    QSqlQuery query;
+    query.exec("CREATE TRIGGER IF NOT EXISTS RequeteEnregistrer AFTER INSERT ON MATERIEL BEGIN INSERT INTO REQUETE ('TypeRequete','DHRequete','UsernameAdmin','UsernameUtilisateur','NomMateriel') VALUES ('Enregistrement',datetime('now','localtime'),NEW.UsernameAdmin,NEW.UsernameAdmin,NEW.NomMateriel); END;");
+}
