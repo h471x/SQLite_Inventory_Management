1b9e7f14b3a639d20afcf1471468d62b24c6bb20
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-20 03:02:42 +0300

    Insert Admin Functions

---
+++ sources/window/insertadmin.cpp
@@ -0,0 +1,40 @@
+#include "headers/window/insertadmin.h"
+#include "ui_insertadmin.h"
+
+InsertAdmin::InsertAdmin(QWidget *parent) :
+    QDialog(parent),
+    InsertAdminUi(new Ui::InsertAdmin)
+{
+    InsertAdminUi->setupUi(this);
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+}
+
+InsertAdmin::~InsertAdmin()
+{
+    delete InsertAdminUi;
+}
+
+void InsertAdmin::on_pushButton_clicked()
+{
+    QString username = InsertAdminUi->username->text();
+    QString nom = InsertAdminUi->nom->text();
+    QString prenom = InsertAdminUi->prenom->text();
+    QString adresse = InsertAdminUi->adresse->text();
+    QString phone = InsertAdminUi->phone->text();
+    QString password = InsertAdminUi->password->text();
+
+    query.prepare("INSERT OR IGNORE INTO ADMIN(UsernameAdmin, Password, NomAdmin, PrenomAdmin, TelephoneAdmin, AdresseAdmin) VALUES(:usernameadmin, :passwordadmin, :nameadmin, :prenom, :phone, :adresse)");
+    query.bindValue(":usernameadmin", username);
+    query.bindValue(":passwordadmin", password);
+    query.bindValue(":nameadmin", nom);
+    query.bindValue(":prenom", prenom);
+    query.bindValue(":phone", phone);
+    query.bindValue(":adresse", adresse);
+
+    if(query.exec()){
+        this->close();
+    }else{
+        qDebug() << "Error : " << query.lastError();
+    }
+}
+
