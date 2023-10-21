4633d656283287de490a6ec4523bfb15f04adc0b
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-21 23:22:13 +0300

    Changed Etat to NomEtat

--- sources/window/insproduct.cpp
+++ sources/window/insproduct.cpp
@@ -91,7 +91,7 @@ void InsertProduct::on_pushButton_clicked()
             if (adminQuery.exec() && adminQuery.next() && adminQuery.value(0).toInt() == 1) {
                 // Insert data into the MATERIEL table
                 QSqlQuery query;
-                query.prepare("INSERT INTO MATERIEL(NomMateriel, Marque, DEnregistrement, IdCategorie, Etat, NomFournisseur, UsernameAdmin) VALUES(:nom, :marque, strftime('%d/%m/%Y', 'now', 'localtime'), :categorie, :etat, :fournisseur, :admin)");
+                query.prepare("INSERT INTO MATERIEL(NomMateriel, Marque, DEnregistrement, IdCategorie, NomEtat, NomFournisseur, UsernameAdmin) VALUES(:nom, :marque, strftime('%d/%m/%Y', 'now', 'localtime'), :categorie, :etat, :fournisseur, :admin)");
                 query.bindValue(":nom", nom);
                 query.bindValue(":marque", marque);
                 query.bindValue(":categorie", categorie);
