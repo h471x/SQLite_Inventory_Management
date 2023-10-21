9b7c94622d7ddeac3b4e7da4ff0f9cce31489c7d
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-21 03:14:44 +0300

    Trigger Delete Class

---
+++ headers/database/init/triggerdelete.h
@@ -0,0 +1,19 @@
+#ifndef TRIGGERDELETEINIT_H
+#define TRIGGERDELETEINIT_H
+
+#include "headers/database/init/dbinit.h"
+
+class TriggerDeleteInit
+{
+public: //Constructor
+    TriggerDeleteInit();
+
+public: //Method
+    void initTriggerDelete();
+
+public: //Method
+    void TriggerDeleteCreate();
+
+};
+
+#endif // TRIGGERDELETEINIT_H
