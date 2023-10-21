5c3bfe9c034cf1ed8e0c9b55c92a8e6348633c64
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-21 18:08:05 +0300

    Location Class Header

---
+++ headers/database/init/dblocation.h
@@ -0,0 +1,20 @@
+#ifndef LOCATIONINIT_H
+#define LOCATIONINIT_H
+
+#include "headers/database/init/dbinit.h"
+
+class LocationInit
+{
+public: //Constructor
+    LocationInit();
+
+public: //Method
+    void initLocation();
+
+public: //Method
+    void tableLocation();
+    void valueLocation();
+
+};
+
+#endif // LOCATIONINIT_H
