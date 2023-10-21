6ed51fe755717e11663dc34138376d5beda2a300
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-21 18:07:48 +0300

    Etat Table

---
+++ sources/database/init/dbetat.cpp
@@ -0,0 +1,18 @@
+#include "headers/database/init/dbetat.h"
+
+EtatInit::EtatInit(){}
+
+void EtatInit::initEtat(){
+    tableEtat();
+    valueEtat();
+}
+
+void EtatInit::tableEtat(){
+    QSqlQuery query;
+    query.exec("CREATE TABLE IF NOT EXISTS ETAT (NomEtat TEXT PRIMARY KEY );");
+}
+
+void EtatInit::valueEtat(){
+    QSqlQuery query;
+    query.exec("INSERT OR IGNORE INTO ETAT VALUES ('Utilisable'),('En panne'),('a Jeter');");
+}
