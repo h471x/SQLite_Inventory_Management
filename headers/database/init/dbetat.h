0f789f417d9eda7d86e31930827aead0939ccf2c
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-21 18:07:32 +0300

    Etat Class header

---
+++ headers/database/init/dbetat.h
@@ -0,0 +1,20 @@
+#ifndef ETATINIT_H
+#define ETATINIT_H
+
+#include "headers/database/init/dbinit.h"
+
+class EtatInit
+{
+public: //Constructor
+    EtatInit();
+
+public: //Method
+    void initEtat();
+
+public: //Method
+    void tableEtat();
+    void valueEtat();
+
+};
+
+#endif // ETATINIT_H
