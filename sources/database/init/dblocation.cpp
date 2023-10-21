ed980897477e9e240cf1f478dfeeead5e29f9904
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-21 18:51:37 +0300

    Edited Insert Location Query

--- sources/database/init/dblocation.cpp
+++ sources/database/init/dblocation.cpp
@@ -10,10 +10,9 @@ void LocationInit::initLocation(){
 void LocationInit::tableLocation(){
     QSqlQuery query;
     query.exec("CREATE TABLE IF NOT EXISTS EMPLACEMENT (NomEmplacement TEXT PRIMARY KEY, Entrepot INTEGER DEFAULT 0 CHECK(Entrepot IN (0,1)));");
-    qDebug() << "Emplcement : " << query.lastError();
 }
 
 void LocationInit::valueLocation(){
     QSqlQuery query;
-    query.exec("INSERT INTO EMPLACEMENT VALUES ('STOCK'), ('SALLE 01');");
+    query.exec("INSERT INTO EMPLACEMENT VALUES ('STOCK'), ('SALLE 01') ;");
 }
